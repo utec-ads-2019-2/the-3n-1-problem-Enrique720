71bcd990cef9c1378fbdc72d959a7437477f6e21
Enrique Sobrados <39936196+Enrique720@users.noreply.github.com>
2019-08-22 18:25:05 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,35 @@
 #include <iostream>
-
 using namespace std;
+int ciclos(int num){
+    int contador=1;
+    while(num != 1){
+        if(num%2==0){
+            num=num/2;
+        }
+        else{
+            num = num*3+1;
+        }
+        contador++;
+    }
+    return contador;
+}
+int main(){
+    int x,y,temp;
+    int max=0;
+    while(true){
+        cin >> x >> y;
+        cout << x << " " << y  << " " ;
+        if(x < y) {
+            swap(x,y);
+        }
+        for(int i = y;  i <= x;i ++){
+                temp = ciclos(i);
+                if(max < temp){
+                    max = temp;
+                }
+            }
 
-int main(int argc, char *argv[]) {
-    return 0;
+        cout << max << endl;
+        max = 0;
+    }
 }
